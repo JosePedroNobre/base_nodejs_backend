9912799ed226fdae4be3e29fff9635c31a8e6b55
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2020-04-06 03:01:37 +0100

    Update verifyToken.js

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
-    const token = req.header('auth-token')
+    const token = req.header('Authorization')
     if (!token) return res.status(401).send("Access Denied")
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        tokenFormatted = token.slice(7, token.length).replace(/['"]+/g, '');
+        const verified = jwt.verify(tokenFormatted, process.env.TOKEN_SECRET)
         req.user = verified;
-        var decoded = jwt.decode(token);
+        var decoded = jwt.decode(tokenFormatted);
         req.userId = decoded.id
         next()
     } catch (error) {
